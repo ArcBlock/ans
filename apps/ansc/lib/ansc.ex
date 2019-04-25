94c36ba0de4750c81314de43b6168cd0711979e6
taotao.lin <cnredink@gmail.com>
2019-04-25 15:19:53 +0800

    delete some unuseful code

--- apps/ansc/lib/ansc.ex
+++ apps/ansc/lib/ansc.ex
@@ -8,26 +8,8 @@ defmodule Ansc do
   @doc """
 
   """
-  def create_domain(domain, zone_info) do
-  end
-
-  @doc """
-
-  """
-  def update_domain(domain, zone_info) do
-  end
-
-  @doc """
-
-  """
   def get_domain(domain) do
     Lib.get_domain(domain)
-  end
-
-  @doc """
-
-  """
-  def list_domain_by_owner(owner) do
   end
 
   # __end_of_module__
